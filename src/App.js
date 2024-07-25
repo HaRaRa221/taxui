c0f701653e17e70dc49b47f1c338f5032dc144f0
HaRaRa221 <113466996+HaRaRa221@users.noreply.github.com>
2024-07-25 18:45:03 -0400

    Change code for several components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 //Chatbot App 
 import React from 'react';
-import { Chatbot} from 'react-chatbot-kit'; //Chatbot function
+import { Chatbot } from 'react-chatbot-kit'; //Chatbot function
+import 'react-chatbot-kit/build/main.css'; //Chatbot css
 
 
 import './App.css';
